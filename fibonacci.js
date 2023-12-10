d3cf35e17ed78d2e7dc958a5c943203114f01b5c
tastas000 <TiiLg0o000@gmail.com>
2023-12-11 00:10:41 +0700

    finish assignments

---
+++ fibonacci.js
@@ -0,0 +1,5 @@
+function fib(n) {
+  if (n === 0) return 0;
+  if (n === 1 || n === 2) return 1;
+  return fib(n-2) + fib(n-1);
+}
---
+++ mergeSort.js
@@ -0,0 +1,31 @@
+function mergeSort(arr, start, end){
+  //when mergeSort is called outside its function
+  if (arguments.length === 1) {
+    start = 0; end = arr.length - 1;
+  }
+
+  if (start >= end) return;
+  const mid = Math.floor((start + end) / 2);
+  mergeSort(arr, start, mid);
+  mergeSort(arr, mid + 1, end);
+  merge(arr, start, mid, end);
+}
+
+function merge(arr, start, mid, end) {
+  const newArr = [];
+  let i = start;
+  let j = mid + 1;
+
+  for (let k = start; k <= end; k++) {
+    //if i or j is out of range, push and increment only the other
+    if (i > mid) {newArr[k] = arr[j++]; continue}
+    if (j > end) {newArr[k] = arr[i++]; continue}
+    //compare(arr[i] to arr[j]) > push(to newArr) > increment(i or j)
+    newArr[k] = arr[i] < arr[j] ? arr[i++] : arr[j++];
+  }
+
+  //copy merged array(or section) to the original array
+  for (let k = start; k <= end; k++) {
+    arr[k] = newArr[k];
+  }
+}
