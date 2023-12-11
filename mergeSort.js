0d9a96de123809104f415665e21fedff327e3339
tastas000 <TiiLg0o000@gmail.com>
2023-12-11 07:58:29 +0700

    refactor code

--- mergeSort.js
+++ mergeSort.js
@@ -1,9 +1,4 @@
-function mergeSort(arr, start, end){
-  //when mergeSort is called outside its function
-  if (arguments.length === 1) {
-    start = 0; end = arr.length - 1;
-  }
-
+function mergeSort(arr, start = 0, end = arr.length - 1) {
   if (start >= end) return;
   const mid = Math.floor((start + end) / 2);
   mergeSort(arr, start, mid);
@@ -18,8 +13,8 @@ function merge(arr, start, mid, end) {
 
   for (let k = start; k <= end; k++) {
     //if i or j is out of range, push and increment only the other
-    if (i > mid) {newArr[k] = arr[j++]; continue}
-    if (j > end) {newArr[k] = arr[i++]; continue}
+    if (i > mid) {newArr[k] = arr[j++]; continue;}
+    if (j > end) {newArr[k] = arr[i++]; continue;}
     //compare(arr[i] to arr[j]) > push(to newArr) > increment(i or j)
     newArr[k] = arr[i] < arr[j] ? arr[i++] : arr[j++];
   }
